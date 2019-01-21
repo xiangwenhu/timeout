523312ed3a6694acff07013f93fd935fef0328c8
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-01-21 14:17:24 +0800

    添加index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+interface Timeout {
+    start(callback: (next: Function) => void): void;
+    cancel(): void;
+    continue(): void;
+    setInterval(interval: number): void;
+    setContext(context: any): void;
+}
+
+declare function timeout(interval: number, context?: any): Timeout;
+
+export default timeout;
