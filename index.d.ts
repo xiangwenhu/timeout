18313113ef6539fc1c9315ec18c76842e819cb70
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-04-11 16:30:04 +0800

    修改fn为callback,并添加setCallback方法

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 interface Timeout {
-    start(callback: (next: Function) => void): void;
-    cancel(): void;
-    continue(): void;
-    setInterval(interval: number): void;
-    setContext(context: any): void;
+  start(callback: (next: Function) => void): void;
+  cancel(): void;
+  continue(): void;
+  setInterval(interval: number): void;
+  setContext(context: any): void;
 }
 
 declare function timeout(interval: number, context?: any): Timeout;
 
 declare module "etimeout" {
-    export = timeout;
+  export = timeout;
 }
--- index.js
+++ index.js
@@ -1,60 +1,76 @@
 var slice = Array.prototype.slice;
 
 function Timeout(interval, context) {
-    this.context = context || undefined;
-    this.ticketId = null;
-    this.interval = interval;
+  this.context = context || undefined;
+  this.ticketId = null;
+  this.interval = interval;
+  this.callback = null;
 }
 Timeout.prototype.cancel = function() {
-    this.interval && clearTimeout(this.ticketId);
+  this.interval && clearTimeout(this.ticketId);
 };
 
 Timeout.prototype.innerStart = function() {
-    var that = this;
-    var next = function() {
-        that.next(function() {
-            that.excute.apply(that, [next.bind(that)]);
-        });
-    };
-    next.apply(this);
+  var that = this;
+  var next = function() {
+    that.next(function() {
+      that.excute.apply(that, [next.bind(that)]);
+    });
+  };
+  next.apply(this);
 };
 
 Timeout.prototype.excute = function() {
-    var params = slice.call(arguments);
-    this.fn.apply(this.context, params);
+  if (!this.hasCallback()) {
+    this.cancel();
+    return;
+  }
+  var params = slice.call(arguments);
+  this.callback.apply(this.context, params);
 };
 
 Timeout.prototype.next = function(cb) {
-    this.ticketId = setTimeout(cb, this.interval);
+  this.ticketId = setTimeout(cb, this.interval);
+};
+
+Timeout.prototype.setCallback = function(callback) {
+  if (typeof callback !== "function" && callback !== null) {
+    throw new Error("callback should be a function");
+  }
+  this.callback = callback;
+};
+
+Timeout.prototype.hasCallback = function() {
+  return typeof this.callback === "function";
 };
 
 function timeout(interval, context) {
-    var instance = new Timeout(interval, context);
-    return {
-        start: function(callback) {
-            instance.cancel();
-            instance.fn = null;
-            if (typeof callback !== "function") {
-                throw new Error("callback should be a function");
-            }
-            instance.fn = callback;
-            instance.innerStart.apply(instance);
-        },
-        cancel: instance.cancel.bind(instance),
-        continue: function() {
-            if (instance.fn) {
-                instance.innerStart.apply(instance);
-            }
-        },
-        setInterval: function(interval) {
-            instance.cancel();
-            instance.interval = interval;
-            instance.innerStart.apply(instance);
-        },
-        setContext: function(context) {
-            instance.context = context;
-        }
-    };
+  var instance = new Timeout(interval, context);
+  return {
+    start: function(callback) {
+      instance.cancel();
+      instance.setCallback(null);
+      if (typeof callback !== "function") {
+        throw new Error("callback should be a function");
+      }
+      instance.setCallback(callback);
+      instance.innerStart.apply(instance);
+    },
+    cancel: instance.cancel.bind(instance),
+    continue: function() {
+      if (instance.hasCallback()) {
+        instance.innerStart.apply(instance);
+      }
+    },
+    setInterval: function(interval) {
+      instance.cancel();
+      instance.interval = interval;
+      instance.innerStart.apply(instance);
+    },
+    setContext: function(context) {
+      instance.context = context;
+    }
+  };
 }
 
 module.exports = timeout;
